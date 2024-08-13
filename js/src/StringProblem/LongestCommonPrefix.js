f6e8b5d8860558866b3186f74586b5f01e142769
Ibrahim II Musabeh <i.musabah92@gmail.com>
2024-08-13 20:04:20 +0800

    add common prefix -JS

---
+++ js/src/StringProblem/LongestCommonPrefix.js
@@ -0,0 +1,22 @@
+
+
+
+
+export function LongestCommonPrefix(list){
+
+	if (list.length ==0) return '';
+
+	let prefix = list[0];
+
+	for(let word of list){
+		
+		while(word.indexOf(prefix) !== 0 ){
+
+			prefix = prefix.substring(0, prefix.length -1);
+
+			if (prefix =='') return '';
+		}
+	}
+	
+	return prefix;
+}
---
+++ js/test/LongestCommonPrefix.test.js
@@ -0,0 +1,8 @@
+
+import { LongestCommonPrefix } from '../src/StringProblem/LongestCommonPrefix';
+
+
+
+test('test',() => {
+	expect(LongestCommonPrefix(["flower", "flow", "flight"])).toEqual('fl');
+});
