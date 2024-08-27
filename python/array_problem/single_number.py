854d3848982b4cbf59a5ee5b207c45dd9806550e
Ibrahim II Musabeh <i.musabah92@gmail.com>
2024-08-27 21:39:38 +0800

    single number -js/py

---
+++ python/array_problem/single_number.py
@@ -0,0 +1,13 @@
+
+
+
+
+def get_single_number(listx):
+
+	result = 0
+
+	for num in listx:
+		
+		result ^= num
+
+	return result
---
+++ python/test/test_single_number.py
@@ -0,0 +1,9 @@
+
+from array_problem.single_number import get_single_number
+
+
+
+
+def test_single_number():
+
+	assert get_single_number([1,1,4,4,5,5,6]) == 6
