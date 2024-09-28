490b0505a7d4600327e404ca70627e6231fb6fab
Ibrahim II Musabeh <i.musabah92@gmail.com>
2024-09-28 13:16:43 +0800

    add jumpGame - get min jump / JS

--- js/src/ArrayProblem/JumpGame.js
+++ js/src/ArrayProblem/JumpGame.js
@@ -19,3 +19,24 @@ export function canJumpGame(list){
 	}
 	return false;
 }
+
+
+export function getMinJump($list){
+
+	let steps = 0;
+	let maxJump = 0;
+	let length = $list.length;
+	let currentJumpEndAt = 0;
+
+	for (let i = 0;i < length; i++) {
+		
+		maxJump = Math.max(maxJump,(i+$list[i]));
+
+		if (currentJumpEndAt === i) {
+			steps++;
+			currentJumpEndAt = maxJump;
+		}
+
+		if (currentJumpEndAt >= (length -1)) return steps;
+	}
+}
--- js/test/JumpGame.test.js
+++ js/test/JumpGame.test.js
@@ -1,9 +1,15 @@
 
-import { canJumpGame } from '../src/ArrayProblem/JumpGame';
+import { canJumpGame, getMinJump } from '../src/ArrayProblem/JumpGame';
 
 
 
 test('canJumpGame',()=>{
 
 	expect(canJumpGame([2,3,1,0,4])).toEqual(true);
+})
+
+
+test('get min jump',()=>{
+
+	expect(getMinJump([2,1])).toEqual(1);
 })
