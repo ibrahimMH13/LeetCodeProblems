b4931948f96e17cd51d1cb8710a67aa32e073c7a
Ibrahim II Musabeh <i.musabah92@gmail.com>
2024-09-28 13:27:32 +0800

    add jumpGame - get min jump / py

--- python/array_problem/jump_game.py
+++ python/array_problem/jump_game.py
@@ -12,3 +12,28 @@ def can_jump(nums):
 
     # After the loop, check if the farthest reachable index is at or beyond the last index
     return steps >= length
+
+
+
+
+def get_min_jump(list):
+
+    steps = 0
+    maxJamp = 0
+    currentJumpEndAt = 0
+    length = len(list)
+
+
+    for k,v in enumerate(list):
+
+        maxJamp = max(maxJamp, (k + v ))
+
+        if currentJumpEndAt == k:
+
+            currentJumpEndAt = maxJamp
+            
+            steps +=1
+
+        if currentJumpEndAt >= (length -1):
+
+            return steps;
--- python/test/test_jump_game.py
+++ python/test/test_jump_game.py
@@ -1,4 +1,5 @@
 from array_problem.jump_game import can_jump
+from array_problem.jump_game import get_min_jump
 
 
 
@@ -8,3 +9,6 @@ def test_jump():
 	assert can_jump([2,3,1,0,4]) == True
 
 
+def test_get_min_jump():
+
+	assert get_min_jump([2,1]) == 1
