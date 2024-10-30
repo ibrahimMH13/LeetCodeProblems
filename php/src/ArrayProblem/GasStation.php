bdf995c6a7684596b3057747ea58aa525a7946f2
Ibrahim II Musabeh <i.musabah92@gmail.com>
2024-10-30 21:21:53 +0800

    Gaz station algo v2php

--- php/src/ArrayProblem/GasStation.php
+++ php/src/ArrayProblem/GasStation.php
@@ -39,4 +39,27 @@ class GasStation{
 		
 		return $currentStation;
 	}
+
+	public function canCompleteV2($gas, $costs){
+
+		$totalGaz = array_sum($gas);
+		$totalCost = array_sum($costs);
+		$currentStation = 0; 
+		$currentGaz = 0;
+
+		if($totalCost > $totalGaz) return -1;
+
+		for ($i=0; $i < count($gas); $i++) { 
+
+			$currentGaz += $gas[$i] - $costs[$i];
+			
+			if ($currentGaz < 0) {
+
+				$currentGaz = 0;
+				$currentStation =$i + 1;
+			}
+		}
+		return $currentStation;
+
+	}
 }
--- php/test/GasStationTestCase.php
+++ php/test/GasStationTestCase.php
@@ -18,4 +18,17 @@ class GasStationTestCase extends BaseTestCase{
 		$this->assertEquals(3, $solution);
 	}
 
+	#hard
+		# [TEST]
+	public function testGasStationCanCompleteCircuitV2(){
+
+
+		$reslove = new GasStation();
+
+		$solution = $reslove->canCompleteV2([1,2,3,4,5], [3,4,5,1,2]);
+
+		$this->assertEquals(3, $solution);
+	}
+
+
 }
