336c391f8d4c5c62c9a33b8c1de368ce96959427
Ibrahim II Musabeh <i.musabah92@gmail.com>
2024-10-30 22:12:01 +0800

    add cpp

---
+++ cpp/HelloWorld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// A nonzero value from int main() indicates failure. Not ev ery operating system and execution
+// environment make use of that return value: Linux/Unix-based environments often do, but //windows-based environments rarely do.
+
+int main(){
+	
+	std::cout << "Hello World\n";
+	return 0;
+}
---
+++ cpp/square.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+void echoSquare(int num){
+
+	std::cout << " num of " << num << " is " << num * num << " \n";
+}
+int main(){
+
+	echoSquare(2);
+	return 0;
+}
