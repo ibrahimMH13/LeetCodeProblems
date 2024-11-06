7f7fdc5c55dc5e05d05a19e6c251dc534ea5254f
Ibrahim II Musabeh <i.musabah92@gmail.com>
2024-11-06 21:10:43 +0800

    getTwoNumber, js

---
+++ js/src/TwoPointer/TwoSumSortedArray.js
@@ -0,0 +1,25 @@
+
+
+
+
+export const getTwoNumber = (numbers, target)=>{
+
+    let left = 0;
+    let right = numbers.length -1;
+
+    while(left <= right){
+
+        let total = numbers[left] + numbers[right];
+
+        if(total === target){
+            return [left + 1, right + 1];
+        }
+
+        if(total > target){
+            right--;
+        }else{
+            left++;
+        }
+    }
+    return [];
+}
---
+++ js/test/TwoSumSortedArray.test.js
@@ -0,0 +1,8 @@
+import { getTwoNumber } from '../src/TwoPointer/TwoSumSortedArray';
+
+
+test('tst Two Number', () => { 
+    
+    expect(getTwoNumber([2,11,7,15], 9)).toEqual([1,3]);
+
+ });
